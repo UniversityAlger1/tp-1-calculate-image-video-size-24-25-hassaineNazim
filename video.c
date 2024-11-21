91fa09272a82b7e4b8a509d75b197d331ce4054f
hassaineNazim <hassaine.nazim2@gmail.com>
2024-11-21 11:53:32 +0100

    Update video.c

--- video.c
+++ video.c
@@ -8,9 +8,28 @@
 //   unit: Unit of the output value. It could be 'bt' byte, 'ko' kilobits, 'mo' megabits, 'go' gigabits
 // Return value
 //   colored video size (based on the unit passed parametter)
+
 float video(int w, int h, int durationMovie, int durationCredits, int fps, char* unit) {
    // YOUR CODE HERE - BEGIN
+   int colorImgSizeBytes = w * h * 3.0; 
+   int bwImgSizeBytes = (w * h1.0)/8;
 
+   float totalSizeBytes = ((float)colorImgSizeBytes durationMovie + (float)bwImgSizeBytes * durationCredits) * fps;
+
+   if (strcmp(unit, "ko") == 0) {
+      return totalSizeBytes / 1024.0;
+   }
+   else if (strcmp(unit, "bt") == 0) {
+      return totalSizeBytes;
+   }
+   else if (strcmp(unit, "mo") == 0) {
+      return totalSizeBytes / (1024.0 * 1024.0);
+   }
+   else if (strcmp(unit, "go") == 0) {
+      return totalSizeBytes / (1024.0 * 1024.0 * 1024.0);
+   }
    // YOUR CODE HERE - END
    return 0;
 }
+
+   
